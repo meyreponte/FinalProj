3719eb769c4bf54739195924cb4876fecf7188a3
meyreponte <157481205+meyreponte@users.noreply.github.com>
2024-05-16 23:16:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -67,8 +67,8 @@ if uploaded_file is not None:
 
     if confidence < confidence_threshold:
         predicted_class = "Unknown"
-    else:
-        predicted_class = class_names[predicted_class]
+    #else:
+        #predicted_class = class_names[predicted_class]
 
     st.write(f"Prediction: {predicted_class}")
     st.write(f"Confidence: {confidence:.2f}")
